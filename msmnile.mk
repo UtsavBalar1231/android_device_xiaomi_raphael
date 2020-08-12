d5bc605edef9d64731cccff205401cce0e291e24
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    raphael: create dummy libqti-perfd-client
        * CameraHAL complains about missing perfd and then crashes, so let's
        * create a dummy libqti-perfd-client to shut up the hal
        * we could even map the functions to use libperfmgr powerhints in future
    
        08-12 10:21:24.212 E/CHIUSECASE(22011): [ERROR  ] chxutils.cpp:628 LibMap() Failed to load library libqti-perfd-client.so error dlopen failed: library libqti-perfd-client.so not found
        08-12 10:21:24.212 E/CHIUSECASE(22011): [ERROR  ] chxextensionmodule.cpp:181 ExtensionModule() Failed to load perf lib
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -130,6 +130,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
