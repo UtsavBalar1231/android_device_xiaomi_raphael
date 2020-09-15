f2998b6b27ce78eb2d9bce7416a1dcdfd8c1da6f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-09-15 12:54:23 +0000

    raphael: copy lineage  inscreen fingerprint permission
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -178,6 +178,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
