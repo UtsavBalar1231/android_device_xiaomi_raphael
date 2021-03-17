981da7593091481e2ff42d1c5f9f28eb36525bd8
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-03-17 13:24:51 +0100

    raphael: Build vendor.qti.hardware.bluetooth_audio@2.1.vendor
    Change-Id: I49c82ba040f9a896d6637b3773834f1b6c00b295

--- device.mk
+++ device.mk
@@ -131,6 +131,7 @@ PRODUCT_PACKAGES += \
     libbthost_if \
     libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
