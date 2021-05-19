13d4f99970f8eed757bc8f3483dd9ac165463074
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-05-19 17:33:42 +0200

    raphael: wifi: Import p2p_supplicant_overlay.conf from redbull
    Change-Id: Icdb4d76d3ca2be3e269600f9495f43ce1b7a1be6

--- device.mk
+++ device.mk
@@ -557,6 +557,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Override heap growth limit due to high display density on device
